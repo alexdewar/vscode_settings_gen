da8f1bbcd0aadbf4018db1660ce3a7966ab69c0b
Alex Dewar <a.dewar@imperial.ac.uk>
2022-09-15 10:08:49 +0100

    Add my overrides

--- overrides.py
+++ overrides.py
@@ -1,3 +1,7 @@
-# Define any options you want to be set programmatically here
+import platform
+win = platform.system() == "Windows"
+
 overrides = {
+    "editor.fontFamily": "Fira Code" if win else "Jetbrains Mono",
+    "terminal.integrated.fontFamily": None if win else "MesloLGS NF",
 }
