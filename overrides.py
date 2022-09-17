197df96c74edb39c8226a36b9f631b57d0e00438
Alex Dewar <a.dewar@imperial.ac.uk>
2022-09-17 18:47:40 +0100

    Don't specify font on Windows

--- overrides.py
+++ overrides.py
@@ -2,6 +2,6 @@ import platform
 win = platform.system() == "Windows"
 
 overrides = {
-    "editor.fontFamily": "Fira Code" if win else "Jetbrains Mono",
+    "editor.fontFamily": None if win else "Jetbrains Mono",
     "terminal.integrated.fontFamily": None if win else "MesloLGS NF",
 }
